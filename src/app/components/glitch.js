25fdd4c67a472b7a4d83693096b13ff5cdf275d7
Holly Phillips <hollymphillips.dev@gmail.com>
2025-12-22 13:08:38 -0800

    first add of project

---
+++ src/app/components/glitch.js
@@ -0,0 +1,228 @@
+import Image from "next/image";
+
+export default function Glitch() {
+    return (
+        <>
+            <div className="bard">
+                <div className="strip" style={{
+                    '--glitch-x-1': -2 + 'em',
+                    '--glitch-hue-1': -16 + 'deg',
+                    '--glitch-x-2': 2 + 'em',
+                    '--glitch-hue-2': 13 + 'deg',
+                    backgroundPosition: '0 -0em',
+                    height: 5 + 'em', 
+                    animationName: 'glitch-9',
+                    animationDuration: 9000 + 'ms', 
+                    animationDelay: 2 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': -1 + 'em',
+                    '--glitch-hue-1': -42 + 'deg',
+                    '--glitch-x-2': -6 + 'em',
+                    '--glitch-hue-2': -23 + 'deg',
+                    backgroundPosition: '0 -5em',
+                    height: 3 + 'em', 
+                    animationName: 'glitch-5',
+                    animationDuration: 5000 + 'ms', 
+                    animationDelay: 1 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': -10 + 'em',
+                    '--glitch-hue-1': -42 + 'deg',
+                    '--glitch-x-2': -9 + 'em',
+                    '--glitch-hue-2': 45 + 'deg',
+                    backgroundPosition: '0 -8em',
+                    height: 3 + 'em', 
+                    animationName: 'glitch-7',
+                    animationDuration: 7000 + 'ms', 
+                    animationDelay: 0 + 's',
+                }}></div>
+                
+                <div className="strip" style={{
+                    '--glitch-x-1': -6 + 'em',
+                    '--glitch-hue-1': -11 + 'deg',
+                    '--glitch-x-2': 2 + 'em',
+                    '--glitch-hue-2': 29 + 'deg',
+                    backgroundPosition: '0 -11em',
+                    height: 5 + 'em', 
+                    animationName: 'glitch-8',
+                    animationDuration: 8000 + 'ms', 
+                    animationDelay: 1 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': 0 + 'em',
+                    '--glitch-hue-1': 42 + 'deg',
+                    '--glitch-x-2': 2 + 'em',
+                    '--glitch-hue-2': -28 + 'deg',
+                    backgroundPosition: '0 -16em',
+                    height: 5 + 'em', 
+                    animationName: 'glitch-6',
+                    animationDuration: 6000 + 'ms', 
+                    animationDelay: 0 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': -3 + 'em',
+                    '--glitch-hue-1': -45 + 'deg',
+                    '--glitch-x-2': -5 + 'em',
+                    '--glitch-hue-2': -15 + 'deg',
+                    backgroundPosition: '0 -21em',
+                    height: 2 + 'em', 
+                    animationName: 'glitch-7',
+                    animationDuration: 7000 + 'ms', 
+                    animationDelay: 0 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': -5 + 'em',
+                    '--glitch-hue-1': 35 + 'deg',
+                    '--glitch-x-2': 9 + 'em',
+                    '--glitch-hue-2': 35 + 'deg',
+
+                    backgroundPosition: 0 -23 + 'em',
+                    height: 2 + 'em', 
+                    animationName: 'glitch-6',
+                    animationDuration: 6000 + 'ms', 
+                    animationDelay: 0 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': 10 + 'em',
+                    '--glitch-hue-1': 39 + 'deg',
+                    '--glitch-x-2': -8 + 'em',
+                    '--glitch-hue-2': 24 + 'deg',
+
+                    backgroundPosition: 0 -25 + 'em',
+                    height: 6 + 'em', 
+                    animationName: 'glitch-7',
+                    animationDuration: 7000 + 'ms', 
+                    animationDelay: 1 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': -6 + 'em',
+                    '--glitch-hue-1': -46 + 'deg',
+                    '--glitch-x-2': -3 + 'em',
+                    '--glitch-hue-2': 18 + 'deg',
+
+                    backgroundPosition: 0 -31 + 'em',
+                    height: 6 + 'em', 
+                    animationName: 'glitch-10',
+                    animationDuration: 10000 + 'ms', 
+                    animationDelay: 1 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': -8 + 'em',
+                    '--glitch-hue-1': -37 + 'deg',
+                    '--glitch-x-2': -6 + 'em',
+                    '--glitch-hue-2': 15 + 'deg',
+
+                    backgroundPosition: 0 -37 + 'em',
+                    height: 1 + 'em', 
+                    animationName: 'glitch-9',
+                    animationDuration: 9000 + 'ms', 
+                    animationDelay: 1 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': -1 + 'em',
+                    '--glitch-hue-1': 16 + 'deg',
+                    '--glitch-x-2': 2 + 'em',
+                    '--glitch-hue-2': 25 + 'deg',
+
+                    backgroundPosition: 0 -38 + 'em',
+                    height: 2 + 'em', 
+                    animationName: 'glitch-5',
+                    animationDuration: 5000 + 'ms', 
+                    animationDelay: 1 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': 5 + 'em',
+                    '--glitch-hue-1': 32 + 'deg',
+                    '--glitch-x-2': 10 + 'em',
+                    '--glitch-hue-2': -3 + 'deg',
+
+                    backgroundPosition: 0 -40 + 'em',
+                    height: 2 + 'em', 
+                    animationName: 'glitch-9',
+                    animationDuration: 9000 + 'ms', 
+                    animationDelay: 1 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': 10 + 'em',
+                    '--glitch-hue-1': -26 + 'deg',
+                    '--glitch-x-2': 6 + 'em',
+                    '--glitch-hue-2': -45 + 'deg',
+
+                    backgroundPosition: 0 -42 + 'em',
+                    height: 4 + 'em', 
+                    animationName: 'glitch-6',
+                    animationDuration: 6000 + 'ms', 
+                    animationDelay: 1 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': -7 + 'em',
+                    '--glitch-hue-1': -45 + 'deg',
+                    '--glitch-x-2': -8 + 'em',
+                    '--glitch-hue-2': 45 + 'deg',
+
+                    backgroundPosition: 0 -46 + 'em',
+                    height: 3 + 'em', 
+                    animationName: 'glitch-5',
+                    animationDuration: 5000 + 'ms', 
+                    animationDelay: 2 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': 4 + 'em',
+                    '--glitch-hue-1': 40 + 'deg',
+                    '--glitch-x-2': -8 + 'em',
+                    '--glitch-hue-2': 29 + 'deg',
+
+                    backgroundPosition: 0 -49 + 'em',
+                    height: 3 + 'em', 
+                    animationName: 'glitch-6',
+                    animationDuration: 6000 + 'ms', 
+                    animationDelay: 1 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': -4 + 'em',
+                    '--glitch-hue-1': 26 + 'deg',
+                    '--glitch-x-2': -6 + 'em',
+                    '--glitch-hue-2': -3 + 'deg',
+
+                    backgroundPosition: 0 -52 + 'em',
+                    height: 6 + 'em', 
+                    animationName: 'glitch-10',
+                    animationDuration: 10000 + 'ms', 
+                    animationDelay: 0 + 's',
+                }}></div>
+
+                <div className="strip" style={{
+                    '--glitch-x-1': 6 + 'em',
+                    '--glitch-hue-1': 43 + 'deg',
+                    '--glitch-x-2': -1 + 'em',
+                    '--glitch-hue-2': -1 + 'deg',
+
+                    backgroundPosition: 0 -58 + 'em',
+                    height: 4 + 'em', 
+                    animationName: 'glitch-8',
+                    animationDuration: 8000 + 'ms', 
+                    animationDelay: 0 + 's',
+                }}></div>
+            </div>
+
+            <div className="debug">
+                <div className="code"></div>
+            </div>
+        </>
+    );
+}

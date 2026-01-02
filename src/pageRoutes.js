b977d6abb0c03fc0002605f995a86773182e1ea2
Holly Phillips <hooleymcknight.codes@gmail.com>
2026-01-02 15:10:24 -0800

    i forgot to add files hahahhahahhaa

---
+++ src/pageRoutes.js
@@ -0,0 +1,12 @@
+/*
+    middleware.js must be changed manually
+*/
+
+const pageRoutes = {
+    'home': '/',
+    'index': '/',
+    'account': '/account',
+    'register': '/register',
+}
+
+export default pageRoutes;
